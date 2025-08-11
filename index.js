5d8022f77df12de6026e2d445a496794e88d8452
xiwiaso-collab <xiwiaso@gmail.com>
2025-08-12 06:28:23 +0700

    Delete index.js

--- index.js
+++
@@ -1,8 +0,0 @@
-export default function Home() {
-  return (
-    <div style={{ textAlign: "center", marginTop: "50px" }}>
-      <h1>Hello from Vercel + GitHub Actions!</h1>
-      <p>This is a sample Next.js app.</p>
-    </div>
-  );
-}
