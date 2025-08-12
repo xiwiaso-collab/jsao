ed1317e1776f96ebf3e9480c5a9a71ea25fbc5bd
xiwiaso-collab <xiwiaso@gmail.com>
2025-08-12 07:51:59 +0700

    Create index.js

---
+++ pages/index.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div style={{ textAlign: "center", marginTop: "50px" }}>
+      <h1>✅ Deploy สำเร็จแล้ว!</h1>
+      <p>ยินดีต้อนรับสู่เว็บของคุณบน Vercel 🚀</p>
+    </div>
+  );
+}
