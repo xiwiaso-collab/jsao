17c4de742cbdc7386b179377847db13bfd42570e
xiwiaso-collab <xiwiaso@gmail.com>
2025-08-14 14:35:54 +0700

    Delete index.js

--- index.js
+++
@@ -1,8 +0,0 @@
-export default function Home() {
-  return (
-    <div style={{textAlign: "center", marginTop: "50px"}}>
-      <h1>✅ LINE Webhook พร้อมทำงาน</h1>
-      <p>Deploy บน Vercel สำเร็จแล้ว</p>
-    </div>
-  );
-}
